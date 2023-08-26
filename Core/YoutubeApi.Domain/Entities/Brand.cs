a27c2ea1c3a4f558c1bae91522e13a72a709b26c
Cem Keskin <Cem@teknohub.com>
2023-08-26 14:58:21 +0300

    temel entityler oluşturuldu

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,23 @@
+﻿using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail()
+        {
+
+        }
+        public Detail(string title, string description, int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public required int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
