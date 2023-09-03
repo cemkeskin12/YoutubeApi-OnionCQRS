b5a354f1a686160586c9d8e46e4db2f665b99fb1
Cem Keskin <Cem@teknohub.com>
2023-09-03 20:32:35 +0300

    Unitofwork yapısı kuruldu.

--- Core/YoutubeApi.Domain/Entities/Category.cs
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -14,9 +14,9 @@ namespace YoutubeApi.Domain.Entities
             Name = name;
             Priorty = priorty;
         }
-        public required int ParentId { get; set; }
-        public required string Name { get; set; }
-        public required int Priorty { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+        public int Priorty { get; set; }
         public ICollection<Detail> Details { get; set; }
         public ICollection<Product> Products { get; set; }
     }
