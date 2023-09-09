cc9647d177bca59d61552caf6614ada15269f58b
Cem Keskin <Cem@teknohub.com>
2023-09-09 15:11:42 +0300

    cqrs'e başladık

---
+++ Core/YoutubeApi.Application/Registration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Application
+{
+    public static class Registration
+    {
+        public static void AddApplication(this IServiceCollection services) 
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+        }
+    }
+}
---
+++ Presentation/YoutubeApi.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeApi.Application.Features.Products.Queries.GetAllProducts;
+
+namespace YoutubeApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await mediator.Send(new GetAllProductsQueryRequest());
+
+            return Ok(response);
+        }
+    }
+}
