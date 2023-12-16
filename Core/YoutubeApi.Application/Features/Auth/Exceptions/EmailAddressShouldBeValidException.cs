1afaec8ad5ab4b4371f4b88568d3e6f52546cba0
Cem Keskin <Cem@teknohub.com>
2023-12-16 14:17:20 +0300

    revoke ve revoke all yapısı kuruldu.

---
+++ Core/YoutubeApi.Application/Features/Auth/Exceptions/EmailAddressShouldBeValidException.cs
@@ -0,0 +1,9 @@
+﻿using YoutubeApi.Application.Bases;
+
+namespace YoutubeApi.Application.Features.Auth.Exceptions
+{
+    public class EmailAddressShouldBeValidException : BaseException
+    {
+        public EmailAddressShouldBeValidException() : base("Böyle bir email adresi bulunmamaktadır.") { }
+    }
+}
